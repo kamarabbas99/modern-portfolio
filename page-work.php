e5286f8339476606431177d8c68eacfc2c5318b9
Kam Saiyed <saiyedkamarabbas64@gmail.com>
2021-01-12 18:18:54 -0800

    add ACF functionaly to projects

--- page-work.php
+++ page-work.php
@@ -12,8 +12,34 @@ get_header();
     <h2 class="sm-heading">
       Check out some of my projects...
     </h2>
+
     <div class="projects">
-      <div class="item">
+
+
+      <?php while (have_rows('projects')) : the_row();
+        $image = get_sub_field('image');
+        $title = get_sub_field('title');
+        $tech = get_sub_field('tech');
+        $gitLink = get_sub_field('gitLink');
+        $projectLink = get_sub_field('projectLink');
+      ?>
+        <img src="<?php echo $image?>" alt="Kam Saiyed" class="bio-image" />
+
+        <div class="item">
+          <a href="<?php echo $gitLink?>">
+            <img src="<?php echo $image?>" alt="Project">
+          </a>
+          <a href="projectLink" class="btn-light">
+            <i class="fas fa-eye"></i> Project
+          </a>
+          <a href="<?php echo $gitLink?>" class="btn-dark">
+            <i class="fab fa-github"></i> Github
+          </a>
+        </div>
+
+      <?php endwhile ; ?>
+
+      <!-- <div class="item">
         <a href="#!">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/projects/project1.jpg" alt="Project">
         </a>
@@ -24,6 +50,7 @@ get_header();
           <i class="fab fa-github"></i> Github
         </a>
       </div>
+
       <div class="item">
         <a href="#!">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/projects/project2.jpg" alt="Project">
@@ -35,6 +62,7 @@ get_header();
           <i class="fab fa-github"></i> Github
         </a>
       </div>
+
       <div class="item">
         <a href="#!">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/projects/project3.jpg" alt="Project">
@@ -46,6 +74,7 @@ get_header();
           <i class="fab fa-github"></i> Github
         </a>
       </div>
+
       <div class="item">
         <a href="#!">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/projects/project4.jpg" alt="Project">
@@ -57,6 +86,7 @@ get_header();
           <i class="fab fa-github"></i> Github
         </a>
       </div>
+
       <div class="item">
         <a href="#!">
           <img src= "<?php echo get_template_directory_uri();?>/assets/images/projects/project5.jpg" alt="Project">
@@ -67,7 +97,8 @@ get_header();
         <a href="#" class="btn-dark">
           <i class="fab fa-github"></i> Github
         </a>
-      </div>
+      </div> -->
+
     </div>
 
     <div id="main-footer">
